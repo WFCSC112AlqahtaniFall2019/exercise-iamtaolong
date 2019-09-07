d8314e0bde1d1e3e2e54f43641e311779714ac93
Tao Long <iamtaolong@gmail.com>
2019-09-07 18:42:34 -0400

    Intial Commit

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+
     const int NUM_ELEMENTS = 8;   // Number of elements
     int revVctr[8];               // User values
     int i;                        // Loop index
@@ -11,17 +12,22 @@ int main() {
         cin >> revVctr[i];
     }
 
+    int temp = 0; //define a temporary number to help change the value
     // Reverse
-    for (i = 0; i < NUM_ELEMENTS; ++i) {
-        revVctr[i] = revVctr[NUM_ELEMENTS - i]; // Swap
+    for (i = 0; i < NUM_ELEMENTS / 2 ; ++i) {
+        temp = revVctr[NUM_ELEMENTS - i - 1];   //subtract another 1 from it to get the right spot
+        revVctr[NUM_ELEMENTS - i - 1] = revVctr[i]; // Swap
+        revVctr[i] = temp;
+
     }
 
     // Print values
     cout << endl << "New values: ";
-    for (i = 0; i < NUM_ELEMENTS; ++i) {
+    for (i = 0; i < NUM_ELEMENTS ; ++i) {
         cout << " " << revVctr[i];
     }
     cout << endl;
 
     return 0;
+
 }
